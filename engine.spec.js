7431026e87d12d42f0f11c58205acc4f34647475
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-03-30 17:54:05 +0530

    Update engine.spec.js

--- engine.spec.js
+++ engine.spec.js
@@ -4,7 +4,7 @@ var engine = require('./engine');
 describe('engine', function () {
     it('should run with no arguments', function () {
         // Although we don't pass in anything, this is enough to test
-        // that our dependencies are actually installed.
+        // that our dependencies are actually installed.ss
         engine({}, {cloud: 'aws'});
     })
 });
